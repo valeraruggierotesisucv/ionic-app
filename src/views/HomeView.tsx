3cc7373e22ebf08829d04e068157493095d9595a
jhoabran <69612177+jhoabran@users.noreply.github.com>
2025-02-19 14:12:07 -0400

    fixing homeview

--- src/views/HomeView.tsx
+++ src/views/HomeView.tsx
@@ -1,18 +1,14 @@
-import React, { useState } from 'react';
+import React, {  } from 'react';
 import { 
   IonButton,
   IonContent, 
   IonHeader, 
-  IonImg, 
   IonPage, 
   IonToolbar,
   IonText
 } from '@ionic/react';
-import { NavigationService } from '../services/NavigationService';
-import { SearchBar } from '../components/SearchBar/SearchBar';
-import NotificationItem from '../components/NotificationItem/NotificationItem';
-import { NotificationType } from '../components/NotificationItem/NotificationItem';
 import { useAuth } from '../contexts/AuthContext';
+import { AppHeader } from '../components/AppHeader/AppHeader';
 
 
 const Tab1Home: React.FC = () => {
@@ -27,22 +23,12 @@ const Tab1Home: React.FC = () => {
 
       <IonContent className="ion-padding">
 
-        <IonButton onClick={() => setShowComments(true)}>
-          Ver comentarios
-        </IonButton>
-
-        <IonButton 
-          expand="block"
-          routerLink={"/login"} 
-          routerDirection="forward"
-        >
-          Go Login
-        </IonButton>
-        
-      </IonContent>
       <IonText>
         {user?.email}
       </IonText>
+        
+        
+      </IonContent>
     </IonPage>
   );
 };
