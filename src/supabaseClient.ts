67380c0061c7248267eb10cad24aa10d2aa90ace
ruggieroDaniela <chachy.drs@gmail.com>
2025-03-06 20:54:45 -0400

    persistencia de sesion

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,6 +1,37 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
+import { App } from '@capacitor/app'; 
+import { Preferences } from '@capacitor/preferences'; 
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: {
+      async getItem(key) {
+        const { value } = await Preferences.get({ key });
+        return value;
+      },
+      async setItem(key, value) {
+        await Preferences.set({ key, value });
+      },
+      async removeItem(key) {
+        await Preferences.remove({ key });
+      },
+    },
+    autoRefreshToken: true, 
+    persistSession: true, 
+    detectSessionInUrl: false, 
+  },
+});
+
+// Manejo del ciclo de vida de la aplicación para actualización automática del token
+App.addListener('appStateChange', ({ isActive }) => {
+  if (isActive) {
+    // Iniciar la actualización automática del token cuando la app está activa
+    supabase.auth.startAutoRefresh();
+  } else {
+    // Detener la actualización automática del token cuando la app está en segundo plano
+    supabase.auth.stopAutoRefresh();
+  }
+});
